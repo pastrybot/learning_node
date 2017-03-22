179bd85f7407209cfaa6a612c346b75b7b54dd88
Katie Dahlin <pastry.katie@gmail.com>
2017-03-21 21:32:29 -0600

    configured VERY BASIC server using http node module

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("hello, world!")
+
+var http = require('http');
+//node module that creates a small server
+//we are making a fucking server
+
+http.createServer(function(request, response){
+  response.end('hello world');
+console.log("Your server is running at port 3000");
+}).listen(3000); //this will start up our server at port 3000
